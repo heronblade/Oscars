6ba935ac75d28e59e6dc79381c1d9dfe14c80924
Joshua Byler <bylerj@gmail.com>
2014-01-27 23:15:40 -0500

    added oscars2

--- oscars/oscars.js
+++ oscars/oscars.js
@@ -1,34 +1,89 @@
-//collections
-bestPicture = new Meteor.Collection('bestPicture');
-josh = new Meteor.Collection('josh');
+// Set up a collection to contain player information. On the server,
+// it is backed by a MongoDB collection named "players".
+
+Players = new Meteor.Collection("players");
+joe = new Meteor.Collection('bestPicture');
 
 if (Meteor.isClient) {
-	Template.bestPicture.person = function() {
-		// console.log(bestPicture.find({}));
-		return bestPicture.find({});
+	Template.bestPicture.nominee = function() {
+		return joe.find({});
 	}
 
-	Template.josh.foods = function() {
-		return josh.find();
-	}
+  Template.leaderboard.josh = function () {
+    return Players.find({}, {sort: {score: -1, name: 1}});
+  };
+
+  Template.leaderboard.selected_name = function () {
+    var player = Players.findOne(Session.get("selected_player"));
+    return player && player.name;
+  };
+
+  Template.player.selected = function () {
+    return Session.equals("selected_player", this._id) ? "selected" : '';
+  };
+
+  Template.leaderboard.events({
+    'click input.inc': function () {
+      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    }
+  });
+
+  Template.player.events({
+    'click': function () {
+      Session.set("selected_player", this._id);
+    }
+  });
 }
 
+// On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
-	if (bestPicture.find().count === 0) {
-		var names = ["Ada Lovelace",
+  Meteor.startup(function () {
+    if (Players.find().count() === 0) {
+      var names = ["Ada Lovelace",
                    "Grace Hopper",
                    "Marie Curie",
                    "Carl Friedrich Gauss",
                    "Nikola Tesla",
                    "Claude Shannon"];
       for (var i = 0; i < names.length; i++)
-        bestPicture.insert({name: names[i]});
-	}
+        Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
+    }
 
-	if (josh.find().count === 0) {
-		console.log('josh is empty');
-	} else {
-		console.log('josh is not empty');
-		josh.insert({food: 'peach'});
-	}
+    if (joe.find().count() === 0) {
+    	var name = [
+                '12 Years A Slave',
+                'American Hustle',
+                'Captain Phillips',
+                'Dallas Buyers Club',
+                'Gravity',
+                'Her',
+                'Nebraska',
+                'Philomena',
+                'The Wolf of Wall Street'];
+        var img = [
+                '12-years-a-slave',
+                'american-hustle',
+                'captain-phillips',
+                'dallas-buyers-club',
+                'gravity',
+                'her',
+                'nebraska',
+                'philomena',
+                'the-wolf-of-wall-street'];
+        var trailer = [
+                'vUQNjfhlREk',
+                'ST7a1aK_lG0',
+                '_3ASoBrFGlc',
+                'fvMPU0WaPcc',
+                'ufsrgE0BYf0',
+                'WzV6mXIOVl4',
+                'UT5tqPojMtg',
+                '4DBPqcp6Hc4',
+                'iszwuX1AK6A'];
+
+        for (var i = 0; i < name.length; i++) {
+        	joe.insert({name: name[i], img: img[i], trailer: trailer[i]});
+        }
+    }
+  });
 }

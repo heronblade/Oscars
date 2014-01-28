eaad4d16f6b56506dfde505ca79f98dd32c07669
Joshua Byler <bylerj@gmail.com>
2014-01-27 23:19:29 -0500

    finally figured out the good old collections

--- oscars/oscars.js
+++ oscars/oscars.js
@@ -1,88 +1,72 @@
-// Set up a collection to contain player information. On the server,
-// it is backed by a MongoDB collection named "players".
-
-Players = new Meteor.Collection("players");
-joe = new Meteor.Collection('bestPicture');
+//collections
+bestPicture = new Meteor.Collection('bestPicture');
+actor = new Meteor.Collection('actor');
+actress = new Meteor.Collection('actress');
+actorSupp = new Meteor.Collection('actorSupp');
+actressSupp = new Meteor.Collection('actressSupp');
+animateFeatFilm = new Meteor.Collection('animateFeatFilm');
+cinematography = new Meteor.Collection('cinematography');
+costumeDesign = new Meteor.Collection('costumeDesign');
+directing = new Meteor.Collection('directing');
+docFeature = new Meteor.Collection('docFeature');
+docShort = new Meteor.Collection('docShort');
+filmEditing = new Meteor.Collection('filmEditing');
+foreignLanguage = new Meteor.Collection('foreignLanguage');
+makeup = new Meteor.Collection('makeup');
+musicOrigScore = new Meteor.Collection('musicOrigScore');
+musicOrigSong = new Meteor.Collection('musicOrigSong');
+prodDesign = new Meteor.Collection('prodDesign');
+shortFilmAnimated = new Meteor.Collection('shortFilmAnimated');
+shortFilmLiveAction = new Meteor.Collection('shortFilmLiveAction');
+soundEditing = new Meteor.Collection('soundEditing');
+soundMixing = new Meteor.Collection('soundMixing');
+visualEffects = new Meteor.Collection('visualEffects');
+writingAdapted = new Meteor.Collection('writingAdapted');
+writingOriginal = new Meteor.Collection('writingOriginal');
 
 if (Meteor.isClient) {
-	Template.bestPicture.nominee = function() {
-		return joe.find({});
-	}
-
-  Template.leaderboard.josh = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
-  };
-
-  Template.leaderboard.selected_name = function () {
-    var player = Players.findOne(Session.get("selected_player"));
-    return player && player.name;
-  };
-
-  Template.player.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
-  };
-
-  Template.leaderboard.events({
-    'click input.inc': function () {
-      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
-    }
-  });
-
-  Template.player.events({
-    'click': function () {
-      Session.set("selected_player", this._id);
-    }
-  });
+  Template.bestPicture.nominee = function() {
+    return bestPicture.find();
+  }
 }
 
-// On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
-      for (var i = 0; i < names.length; i++)
-        Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
-    }
-
-    if (joe.find().count() === 0) {
-    	var name = [
-                '12 Years A Slave',
-                'American Hustle',
-                'Captain Phillips',
-                'Dallas Buyers Club',
-                'Gravity',
-                'Her',
-                'Nebraska',
-                'Philomena',
-                'The Wolf of Wall Street'];
-        var img = [
-                '12-years-a-slave',
-                'american-hustle',
-                'captain-phillips',
-                'dallas-buyers-club',
-                'gravity',
-                'her',
-                'nebraska',
-                'philomena',
-                'the-wolf-of-wall-street'];
-        var trailer = [
-                'vUQNjfhlREk',
-                'ST7a1aK_lG0',
-                '_3ASoBrFGlc',
-                'fvMPU0WaPcc',
-                'ufsrgE0BYf0',
-                'WzV6mXIOVl4',
-                'UT5tqPojMtg',
-                '4DBPqcp6Hc4',
-                'iszwuX1AK6A'];
+    // code to run on server at startup
+    if (bestPicture.find().count() === 0) {
+      var name = [
+        '12 Years A Slave',
+        'American Hustle',
+        'Captain Phillips',
+        'Dallas Buyers Club',
+        'Gravity',
+        'Her',
+        'Nebraska',
+        'Philomena',
+        'The Wolf of Wall Street'];
+      var img = [
+        '12-years-a-slave',
+        'american-hustle',
+        'captain-phillips',
+        'dallas-buyers-club',
+        'gravity',
+        'her',
+        'nebraska',
+        'philomena',
+        'the-wolf-of-wall-street'];
+      var trailer = [
+        'vUQNjfhlREk',
+        'ST7a1aK_lG0',
+        '_3ASoBrFGlc',
+        'fvMPU0WaPcc',
+        'ufsrgE0BYf0',
+        'WzV6mXIOVl4',
+        'UT5tqPojMtg',
+        '4DBPqcp6Hc4',
+        'iszwuX1AK6A'];
 
         for (var i = 0; i < name.length; i++) {
-        	joe.insert({name: name[i], img: img[i], trailer: trailer[i]});
+          bestPicture.insert({name: name[i], img: img[i], trailer: trailer[i]});
         }
     }
   });

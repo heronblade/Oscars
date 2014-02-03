509e9c0b9a8b792799bec62d9ef7a8e1e893dfca
Joshua Byler <bylerj@gmail.com>
2014-02-03 00:01:00 -0500

    working on autorun feature

--- oscars/login.js
+++ oscars/login.js
@@ -1,5 +1,16 @@
 if (Meteor.isClient) {
 
+	Deps.autorun(function (user) {
+		if (Meteor.user()) {
+			// console.log('user is logged in');
+			user.stop();
+			userIsLoggedIn();
+		}
+	});
+
+	function userIsLoggedIn() {
+		
+	}
 }
 
 if (Meteor.isServer) {

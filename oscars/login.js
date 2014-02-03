148507cd765d7b3f51f772c229679f4996dd728e
Joshua Byler <bylerj@gmail.com>
2014-02-03 14:34:55 -0500

    added rooms test
    I have to come up with a way of grouping players together.  that way
    multiple groups can use the application without interfering with each
    other

--- oscars/login.js
+++ oscars/login.js
@@ -2,18 +2,25 @@ if (Meteor.isClient) {
 
 	Deps.autorun(function (user) {
 		if (Meteor.user()) {
-			// console.log('user is logged in');
 			user.stop();
 			userIsLoggedIn();
+		} else {
+			userIsLoggedOut();
 		}
 	});
 
 	function userIsLoggedIn() {
-
+		console.log('user is logged in');
 		$('#login-background').fadeOut(500, function() {
 			$('#permanent-header').fadeIn('slow');
 		});
+	}
 
+	function userIsLoggedOut() {
+		console.log('user is logged out');
+		$('#permanent-header').fadeOut(500, function() {
+			$('#login-background').fadeIn('slow');
+		});
 	}
 }
 
